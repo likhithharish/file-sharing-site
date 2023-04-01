b5c97ddb9cdca81cf774b1cd79cf69b0ac5a5533
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-04-01 17:14:36 -0400

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -4,8 +4,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const DBConnection = async () => {
-  const USERNAME = process.env.DB_USERNAME;
-  const PASSWORD = process.env.DB_PASSWORD;
+  const USERNAME = 'test';
+  const PASSWORD = 'test123';
 
   const MONGO_URI = `mongodb://${USERNAME}:${PASSWORD}@ac-fkdgikz-shard-00-00.jduufc6.mongodb.net:27017,ac-fkdgikz-shard-00-01.jduufc6.mongodb.net:27017,ac-fkdgikz-shard-00-02.jduufc6.mongodb.net:27017/?ssl=true&replicaSet=atlas-3s9fzo-shard-0&authSource=admin&retryWrites=true&w=majority`;
   try {
